cdcaa2d7eb6e462b19c0c2df4a7095b56ebb3a9e
Aurora Borealis Core <auroraborealiscoin@gmail.com>
2026-09-14 11:31:04 +0200

    ci: fix xcb-proto build with Python 3.12

--- depends/packages/xcb_proto.mk
+++ depends/packages/xcb_proto.mk
@@ -3,10 +3,15 @@ $(package)_version=1.10
 $(package)_download_path=http://xcb.freedesktop.org/dist
 $(package)_file_name=xcb-proto-$($(package)_version).tar.bz2
 $(package)_sha256_hash=7ef40ddd855b750bc597d2a435da21e55e502a0fefa85b274f2c922800baaf05
+$(package)_patches=python312-importlib.patch
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-shared
   $(package)_config_opts_linux=--with-pic
+endef
+
+define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/python312-importlib.patch
 endef
 
 define $(package)_config_cmds
